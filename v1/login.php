67d5dce8e835ca43571584485ab633b06641b2de
Edvin <reci_edvin@hotmail.com>
2025-04-08 23:27:29 +0200

    Add files via upload

---
+++ v1/login.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    if ($username === 'admin' && $password === 'password') {
+        $_SESSION['username'] = $username;
+        header("Location: index.php");
+        exit();
+    } else {
+        $error = "Invalid credentials.";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h2>Login</h2>
+<?php if (isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
+<form method="post">
+    Username: <input type="text" name="username"><br>
+    Password: <input type="password" name="password"><br>
+    <input type="submit" value="Login">
+</form>
+</body>
+</html>
