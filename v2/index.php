9e93752b7ba4fd3f0a1feb0836048dcc32a9758f
Edvin <reci_edvin@hotmail.com>
2025-04-14 16:06:34 +0200

    Update index.php

--- v2/index.php
+++ v2/index.php
@@ -12,7 +12,8 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Mirësevjen, <?php echo $_SESSION['username']; ?>!</h1>
+#    <h1>Mirësevjen, <?php echo $_SESSION['username']; ?>!</h1>
+    <h1>Mirësevjen, admin!</h1>
     <p><a href="members.php">Kliko këtu për të parë listën e pjesmarrësve të klasës</a></p>
     <p><a href="logout.php">Logout</a></p>
 </body>
