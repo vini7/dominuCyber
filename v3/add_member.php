1e20e02c5d805344e05b285ade05e617414796dc
Edvin <reci_edvin@hotmail.com>
2025-04-14 17:13:36 +0200

    Update add_member.php

--- v3/add_member.php
+++ v3/add_member.php
@@ -8,7 +8,7 @@ require_once 'db.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $emri = $_POST['emri'];
     $mbiemri = $_POST['mbiemri'];
-    $conn->query("INSERT INTO users (emri,mbiemri) VALUES ('$emri','$mbiemri')");
+    $conn->query("INSERT INTO users (emri,mbiemri,mosha) VALUES ('$emri','$mbiemri','$mosha')");
     header("Location: members.php");
     exit();
 }
@@ -24,6 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <form method="post">
         Emri: <input type="text" name="emri" required><br>
         Mbiemri: <input type="text" name="mbiemri" required><br>
+        Mosha: <input type="text" name="mosha" required><br>
         <input type="submit" value="Shto">
     </form>
     <p><a href="members.php">Kthehu te lista</a></p>
