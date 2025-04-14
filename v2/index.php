e777936fd215e4d279162893bf53268f202f8095
Edvin <reci_edvin@hotmail.com>
2025-04-14 16:05:20 +0200

    Update index.php

--- v2/index.php
+++ v2/index.php
@@ -1,9 +1,9 @@
 <?php
-session_start();
-if (!isset($_SESSION['username'])) {
-    header("Location: login.php");
-    exit();
-}
+#session_start();
+#if (!isset($_SESSION['username'])) {
+#    header("Location: login.php");
+#    exit();
+#}
 ?>
 <!DOCTYPE html>
 <html>
