f85ceae88390332921ff2baa96452ac455722c7b
Edvin <reci_edvin@hotmail.com>
2025-04-14 15:49:42 +0200

    Update db.php

--- v3/db.php
+++ v3/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'lampuser', 'securepass', 'course_app');
+$conn = new mysqli('localhost', 'user', 'pass', 'db');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
