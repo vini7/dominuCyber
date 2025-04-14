919fcd4bb8bf7a0df3625f681e468f81b15e71c6
Edvin <reci_edvin@hotmail.com>
2025-04-14 16:51:01 +0200

    Update db.php

--- v3/db.php
+++ v3/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'user', 'pass', 'db');
+$conn = new mysqli('localhost', 'admin', 'CyberDB2025', 'klasa_db');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
