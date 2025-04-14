fe4e46ca93bb4fa2db6f259c6e48d306142a1e60
Edvin <reci_edvin@hotmail.com>
2025-04-14 16:53:28 +0200

    Update add_member.php

--- v3/add_member.php
+++ v3/add_member.php
@@ -1,4 +1,4 @@
-<?php
+i<?php
 session_start();
 if (!isset($_SESSION['username'])) {
     header("Location: login.php");
@@ -6,8 +6,8 @@ if (!isset($_SESSION['username'])) {
 }
 require_once 'db.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = $_POST['name'];
-    $conn->query("INSERT INTO members (name) VALUES ('$name')");
+    $name = $_POST['emri'];
+    $conn->query("INSERT INTO members (emri) VALUES ('$emri')");
     header("Location: members.php");
     exit();
 }
@@ -21,8 +21,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
     <h1>Shto Person</h1>
     <form method="post">
-        Name: <input type="text" name="name" required><br>
-        <input type="submit" value="Add">
+        Emri: <input type="text" name="emri" required><br>
+        <input type="submit" value="Shto">
     </form>
     <p><a href="members.php">Kthehu te lista</a></p>
 </body>
